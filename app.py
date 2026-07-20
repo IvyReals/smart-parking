a35cea185a0aeb633303376b210d41b325509483
IvyReals <iniraoooo07@gmail.com>
2026-07-20 14:50:13 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,510 @@
+from flask import Flask, render_template, request, redirect
+import mysql.connector
+from datetime import datetime, timedelta
+
+app = Flask(__name__)
+
+# ------------------------------------
+# DATABASE CONNECTION
+# ------------------------------------
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="smart_parking"
+)
+
+cursor = db.cursor(dictionary=True)
+
+# ------------------------------------
+# HOME
+# ------------------------------------
+
+@app.route("/")
+def home():
+    return render_template("login.html")
+
+# ------------------------------------
+# DASHBOARD
+# ------------------------------------
+
+@app.route("/dashboard")
+def dashboard():
+
+    cursor.execute("SELECT COUNT(*) total FROM user")
+    users = cursor.fetchone()["total"]
+
+    cursor.execute("SELECT COUNT(*) total FROM vehicle")
+    vehicles = cursor.fetchone()["total"]
+
+    cursor.execute("SELECT COUNT(*) total FROM parking_slot")
+    slots = cursor.fetchone()["total"]
+
+    cursor.execute("""
+        SELECT COUNT(*) total
+        FROM booking
+        WHERE bookingStatus='Active'
+    """)
+    bookings = cursor.fetchone()["total"]
+
+    cursor.execute("""
+        SELECT IFNULL(SUM(amount),0) revenue
+        FROM payment
+        WHERE paymentStatus='Paid'
+    """)
+    revenue = cursor.fetchone()["revenue"]
+
+    return render_template(
+        "dashboard.html",
+        users=users,
+        vehicles=vehicles,
+        slots=slots,
+        bookings=bookings,
+        revenue=revenue
+    )
+
+# ------------------------------------
+# USERS
+# ------------------------------------
+
+@app.route("/users")
+def users():
+
+    cursor.execute("""
+        SELECT *
+        FROM user
+        ORDER BY userID
+    """)
+
+    users = cursor.fetchall()
+
+    return render_template(
+        "users.html",
+        users=users
+    )
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+
+    name = request.form["name"]
+    email = request.form["email"]
+    phone = request.form["phone"]
+    password = request.form["password"]
+
+    cursor.execute("""
+
+        INSERT INTO user
+        (name,email,phone,password)
+
+        VALUES
+        (%s,%s,%s,%s)
+
+    """,(name,email,phone,password))
+
+    db.commit()
+
+    return redirect("/users")
+
+# ------------------------------------
+# VEHICLES
+# ------------------------------------
+
+@app.route("/vehicles")
+def vehicles():
+
+    cursor.execute("""
+
+        SELECT
+        vehicle.*,
+        user.name
+
+        FROM vehicle
+
+        JOIN user
+
+        ON vehicle.userID=user.userID
+
+        ORDER BY vehicle.vehicleNo
+
+    """)
+
+    vehicles = cursor.fetchall()
+
+    return render_template(
+        "vehicles.html",
+        vehicles=vehicles
+    )
+
+@app.route("/add_vehicle",methods=["POST"])
+def add_vehicle():
+
+    vehicleNo=request.form["vehicleNo"]
+    userID=request.form["userID"]
+    vehicleType=request.form["vehicleType"]
+    model=request.form["model"]
+    color=request.form["color"]
+
+    cursor.execute("""
+
+        INSERT INTO vehicle
+
+        (
+        vehicleNo,
+        userID,
+        vehicleType,
+        model,
+        color
+        )
+
+        VALUES
+        (%s,%s,%s,%s,%s)
+
+    """,(vehicleNo,userID,vehicleType,model,color))
+
+    db.commit()
+
+    return redirect("/vehicles")
+# ------------------------------------
+# PARKING SLOTS
+# ------------------------------------
+
+@app.route("/slots")
+def slots():
+
+    cursor.execute("""
+
+        SELECT
+        *
+        FROM parking_slot
+        ORDER BY slotLabel
+
+    """)
+
+    slots = cursor.fetchall()
+
+    return render_template(
+        "slots.html",
+        slots=slots
+    )
+
+# ------------------------------------
+# BOOK SLOT
+# ------------------------------------
+
+@app.route("/book_slot", methods=["POST"])
+def book_slot():
+    
+    slotID = request.form["slotID"]
+    vehicleNo = request.form["vehicleNo"]
+    duration = int(request.form["duration"])
+    arrival = request.form["arrival"]
+
+    startTime = datetime.strptime(
+        arrival,
+        "%Y-%m-%dT%H:%M"
+    )
+    
+
+    endTime = startTime + timedelta(hours=duration)
+
+    # -----------------------------
+    # Check Slot
+    # -----------------------------
+
+    cursor.execute("""
+
+    SELECT status,type,rate
+
+    FROM parking_slot
+
+    WHERE slotID=%s
+
+    """,(slotID,))
+
+    slot = cursor.fetchone()
+
+    if slot["type"] != vehicleType:
+        return f"This slot is only for {slot['type']}."
+
+    # -----------------------------
+    # Booking
+    # -----------------------------
+
+    cursor.execute("""
+
+        INSERT INTO booking
+
+        (
+        slotID,
+        vehicleNo,
+        startTime,
+        endTime,
+        bookingStatus
+        )
+
+        VALUES
+
+        (
+        %s,
+        %s,
+        %s,
+        %s,
+        'Active'
+        )
+
+    """,
+
+    (
+        slotID,
+        vehicleNo,
+        startTime,
+        endTime
+    ))
+
+    db.commit()
+
+    bookingID = cursor.lastrowid
+
+    # -----------------------------
+    # Payment
+    # -----------------------------
+
+    amount = float(slot["rate"]) * duration
+
+    cursor.execute("""
+
+        INSERT INTO payment
+
+        (
+        bookingID,
+        amount,
+        method,
+        paymentStatus
+        )
+
+        VALUES
+
+        (
+        %s,
+        %s,
+        'UPI',
+        'Paid'
+        )
+
+    """,
+
+    (
+        bookingID,
+        amount
+    ))
+
+    db.commit()
+
+    # -----------------------------
+    # Occupy Slot
+    # -----------------------------
+
+    cursor.execute("""
+
+        UPDATE parking_slot
+
+        SET status='Occupied'
+
+        WHERE slotID=%s 
+
+    """,(slotID,))
+
+    db.commit()
+
+    return redirect("/bookings")
+
+# ------------------------------------
+# RELEASE SLOT
+# ------------------------------------
+
+@app.route("/release/<int:id>")
+def release(id):
+
+    cursor.execute("""
+
+        SELECT slotID
+
+        FROM booking
+
+        WHERE bookingID=%s
+
+    """,(id,))
+
+    booking = cursor.fetchone()
+
+    if booking:
+
+        cursor.execute("""
+
+            UPDATE booking
+
+            SET bookingStatus='Completed'
+
+            WHERE bookingID=%s
+
+        """,(id,))
+
+        cursor.execute("""
+
+            UPDATE parking_slot
+
+            SET status='Available'
+
+            WHERE slotID=%s
+
+        """,(booking["slotID"],))
+
+        db.commit()
+
+    return redirect("/bookings")
+# ------------------------------------
+# BOOKINGS
+# ------------------------------------
+
+@app.route("/bookings")
+def bookings():
+
+    cursor.execute("""
+
+        SELECT
+
+        booking.bookingID,
+
+        vehicle.vehicleNo,
+
+        parking_slot.slotLabel,
+
+        booking.startTime,
+
+        booking.endTime,
+
+        booking.bookingStatus,
+
+        payment.amount,
+
+        payment.paymentStatus
+
+        FROM booking
+
+        JOIN vehicle
+
+        ON booking.vehicleNo = vehicle.vehicleNo
+
+        JOIN parking_slot
+
+        ON booking.slotID = parking_slot.slotID
+
+        LEFT JOIN payment
+
+        ON booking.bookingID = payment.bookingID
+
+        ORDER BY booking.bookingID DESC
+
+    """)
+
+    bookings = cursor.fetchall()
+
+    return render_template(
+        "bookings.html",
+        bookings=bookings
+    )
+
+# ------------------------------------
+# PAYMENTS
+# ------------------------------------
+
+@app.route("/payments")
+def payments():
+
+    cursor.execute("""
+
+        SELECT
+
+        payment.paymentID,
+
+        payment.bookingID,
+
+        vehicle.vehicleNo,
+
+        parking_slot.slotLabel,
+
+        payment.amount,
+
+        payment.method,
+
+        payment.paymentStatus,
+
+        payment.timestamp
+
+        FROM payment
+
+        JOIN booking
+
+        ON payment.bookingID = booking.bookingID
+
+        JOIN vehicle
+
+        ON booking.vehicleNo = vehicle.vehicleNo
+
+        JOIN parking_slot
+
+        ON booking.slotID = parking_slot.slotID
+
+        ORDER BY payment.paymentID DESC
+
+    """)
+
+    payments = cursor.fetchall()
+
+    return render_template(
+        "payments.html",
+        payments=payments
+    )
+
+# ------------------------------------
+# DELETE USER
+# ------------------------------------
+
+@app.route("/delete_user/<int:id>")
+def delete_user(id):
+
+    cursor.execute(
+        "DELETE FROM user WHERE userID=%s",
+        (id,)
+    )
+
+    db.commit()
+
+    return redirect("/users")
+
+# ------------------------------------
+# DELETE VEHICLE
+# ------------------------------------
+
+@app.route("/delete_vehicle/<vehicleNo>")
+def delete_vehicle(vehicleNo):
+
+    cursor.execute(
+        "DELETE FROM vehicle WHERE vehicleNo=%s",
+        (vehicleNo,)
+    )
+
+    db.commit()
+
+    return redirect("/vehicles")
+
+# ------------------------------------
+# RUN
+# ------------------------------------
+
+if __name__ == "__main__":
+
+    app.run(
+        debug=True
+    )
---
+++ database.sql
@@ -0,0 +1,217 @@
+CREATE DATABASE smart_parking;
+USE smart_parking;
+CREATE TABLE user (
+ userID INT AUTO_INCREMENT PRIMARY KEY,
+ name VARCHAR(100) NOT NULL,
+ email VARCHAR(100) NOT NULL UNIQUE,
+ phone VARCHAR(15) NOT NULL,
+ password VARCHAR(255) NOT NULL
+);
+CREATE TABLE vehicle (
+ vehicleNo VARCHAR(20) PRIMARY KEY,
+ userID INT NOT NULL,
+ vehicleType VARCHAR(50) NOT NULL,
+ model VARCHAR(50),
+ color VARCHAR(30),
+ FOREIGN KEY (userID) REFERENCES user(userID)
+ ON DELETE CASCADE
+ ON UPDATE CASCADE);
+ CREATE TABLE parking_slot (
+ slotID INT AUTO_INCREMENT PRIMARY KEY,
+ slotLabel VARCHAR(20) NOT NULL,
+ status VARCHAR(20) NOT NULL DEFAULT 'Available',
+ type VARCHAR(30) NOT NULL,
+ rate DECIMAL(10,2) NOT NULL
+);
+CREATE TABLE booking (
+ bookingID INT AUTO_INCREMENT PRIMARY KEY,
+ slotID INT NOT NULL,
+ vehicleNo VARCHAR(20) NOT NULL,
+ startTime DATETIME NOT NULL,
+ endTime DATETIME,
+ bookingStatus VARCHAR(20) NOT NULL DEFAULT 'Active',
+ FOREIGN KEY (slotID) REFERENCES parking_slot(slotID)
+ ON DELETE CASCADE
+ ON UPDATE CASCADE,
+ FOREIGN KEY (vehicleNo) REFERENCES vehicle(vehicleNo)
+ ON DELETE CASCADE
+ ON UPDATE CASCADE
+);
+CREATE TABLE payment (
+ paymentID INT AUTO_INCREMENT PRIMARY KEY,
+ bookingID INT NOT NULL,
+ amount DECIMAL(10,2) NOT NULL,
+ method VARCHAR(30) NOT NULL,
+ paymentStatus VARCHAR(20) NOT NULL DEFAULT 'Pending',
+ timestamp DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ FOREIGN KEY (bookingID) REFERENCES booking(bookingID)
+ ON DELETE CASCADE
+ ON UPDATE CASCADE
+);
+CREATE TABLE admin (
+ adminID INT AUTO_INCREMENT PRIMARY KEY,
+ name VARCHAR(100) NOT NULL,
+ email VARCHAR(100) NOT NULL UNIQUE,
+ password VARCHAR(255) NOT NULL,
+ role VARCHAR(50) NOT NULL DEFAULT 'Manager'
+);
+INSERT INTO user (name, email, phone, password) VALUES
+('Rahul Sharma', 'rahul@email.com', '9876543210', 'rahul@123'),
+('Priya Verma', 'priya@email.com', '9876543211', 'priya@456'),
+('Amit Kumar', 'amit@email.com', '9876543212', 'amit@789'),
+('Sneha Reddy', 'sneha@email.com', '9876543213', 'sneha@321'),
+('Vikram Singh', 'vikram@email.com', '9876543214', 'vikram@654');
+
+INSERT INTO vehicle (vehicleNo, userID, vehicleType, model, color) VALUES
+('KA01AB1234', 1, 'Four-Wheeler', 'Maruti Swift', 'White'),
+('KA01CD5678', 1, 'Two-Wheeler', 'Honda Activa', 'Black'),
+('KA02EF9012', 2, 'Four-Wheeler', 'Hyundai i20', 'Silver'),
+('KA03GH3456', 3, 'Two-Wheeler', 'TVS Jupiter', 'Blue'),
+('KA04IJ7890', 4, 'Four-Wheeler', 'Tata Nexon', 'Red'),
+('KA05KL2345', 5, 'Two-Wheeler', 'Bajaj Pulsar', 'Black');
+
+INSERT INTO parking_slot (slotLabel, status, type, rate) VALUES
+('A1', 'Available', 'Four-Wheeler', 50.00),
+('A2', 'Available', 'Four-Wheeler', 50.00),
+('A3', 'Occupied', 'Four-Wheeler', 50.00),
+('B1', 'Available', 'Two-Wheeler', 20.00),
+('B2', 'Available', 'Two-Wheeler', 20.00),
+('B3', 'Occupied', 'Two-Wheeler', 20.00),
+('C1', 'Available', 'Heavy Vehicle', 100.00),
+('C2', 'Reserved', 'Heavy Vehicle', 100.00);
+
+INSERT INTO booking (slotID, vehicleNo, startTime, endTime, bookingStatus) VALUES
+(1, 'KA01AB1234', '2025-06-01 09:00:00', '2025-06-01 12:00:00', 'Completed'),
+(4, 'KA01CD5678', '2025-06-01 10:00:00', '2025-06-01 11:30:00', 'Completed'),
+(2, 'KA02EF9012', '2025-06-02 08:00:00', '2025-06-02 14:00:00', 'Completed'),
+(5, 'KA03GH3456', '2025-06-02 09:30:00', NULL, 'Active'),
+(3, 'KA04IJ7890', '2025-06-03 07:00:00', '2025-06-03 10:00:00', 'Completed'),
+(1, 'KA05KL2345', '2025-06-03 11:00:00', NULL, 'Active');
+
+INSERT INTO payment (bookingID, amount, method, paymentStatus, timestamp) VALUES
+(1, 150.00, 'Card', 'Paid', '2025-06-01 12:05:00'),
+(2, 30.00, 'Cash', 'Paid', '2025-06-01 11:35:00'),
+(3, 300.00, 'Online', 'Paid', '2025-06-02 14:10:00'),
+(5, 150.00, 'Card', 'Paid', '2025-06-03 10:05:00');
+
+INSERT INTO admin (name, email, password, role) VALUES
+('Admin One', 'admin1@parking.com', 'admin@123', 'Super Admin'),
+('Admin Two', 'admin2@parking.com', 'admin@456', 'Manager');
+
+
+
+
+
+
+CREATE TRIGGER after_booking_insert
+AFTER INSERT ON booking
+FOR EACH ROW
+BEGIN
+ UPDATE parking_slot
+ SET status = 'Occupied'
+ WHERE slotID = NEW.slotID
+END //
+DELIMITER ;
+DELIMITER //
+DELIMITER //
+
+CREATE TRIGGER before_booking_insert
+BEFORE INSERT ON booking
+FOR EACH ROW
+BEGIN
+    DECLARE slotStatus VARCHAR(20);
+
+    SELECT status
+    INTO slotStatus
+    FROM parking_slot
+    WHERE slotID = NEW.slotID;
+
+    IF slotStatus = 'Occupied' THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Error: This parking slot is already occupied.';
+    END IF;
+END //
+
+DELIMITER ;
+DELIMITER //
+
+CREATE PROCEDURE RegisterUser(
+    IN p_name VARCHAR(100),
+    IN p_email VARCHAR(100),
+    IN p_phone VARCHAR(15),
+    IN p_password VARCHAR(255)
+)
+BEGIN
+    INSERT INTO user (name, email, phone, password)
+    VALUES (p_name, p_email, p_phone, p_password);
+END //
+
+DELIMITER ;
+CALL RegisterUser('Arun Das', 'arun@email.com', '9876543215', 'arun@999');
+DELIMITER //
+CREATE PROCEDURE CreateBooking(
+	 IN p_slotID INT,
+	 IN p_vehicleNo VARCHAR(20),
+	 IN p_startTime DATETIME
+)
+BEGIN
+ INSERT INTO booking (slotID, vehicleNo, startTime, bookingStatus)
+ VALUES (p_slotID, p_vehicleNo, p_startTime, 'Active');
+END //
+DELIMITER ;
+CALL CreateBooking(2, 'KA02EF9012', '2025-06-04 08:00:00');
+
+DELIMITER //
+CREATE PROCEDURE CompleteBooking(
+	 IN p_bookingID INT,
+	 IN p_method VARCHAR(30)
+)
+BEGIN
+	 DECLARE p_slotID INT;
+	 DECLARE p_startTime DATETIME;
+	 DECLARE p_endTime DATETIME;
+	 DECLARE p_rate DECIMAL(10,2);
+	 DECLARE p_hours INT;
+	 DECLARE p_amount DECIMAL(10,2);
+	 SET p_endTime = NOW();
+	 SELECT b.slotID, b.startTime, ps.rate
+	 INTO p_slotID, p_startTime, p_rate
+	 FROM booking b
+	 JOIN parking_slot ps ON b.slotID = ps.slotID
+	 WHERE b.bookingID = p_bookingID;
+	 SET p_hours = CEIL(TIMESTAMPDIFF(MINUTE, p_startTime, p_endTime) / 60);
+	 SET p_amount = p_hours * p_rate;
+	 UPDATE booking
+	 SET bookingStatus = 'Completed', endTime = p_endTime
+	 WHERE bookingID = p_bookingID;
+	 INSERT INTO payment (bookingID, amount, method, paymentStatus, timestamp)
+	 VALUES (p_bookingID, p_amount, p_method, 'Paid', p_endTime);
+END //
+DELIMITER ;
+
+CALL CompleteBooking(4, 'Online');
+DELIMITER //
+CREATE PROCEDURE GetAvailableSlots(
+	IN p_type VARCHAR(30)
+)
+BEGIN
+	 SELECT slotID, slotLabel, type, rate
+	 FROM parking_slot
+	 WHERE status = 'Available' AND type = p_type;
+END //
+DELIMITER ;
+CALL GetAvailableSlots('Four-Wheeler');
+
+CREATE VIEW active_bookings AS
+
+CREATE VIEW payment_summary AS
+
+
+CREATE VIEW slot_occupancy AS
+SELECT type,
+	 COUNT(*) AS totalSlots,
+	 SUM(CASE WHEN status = 'Available' THEN 1 ELSE 0 END) AS availableSlots,
+	 SUM(CASE WHEN status = 'Occupied' THEN 1 ELSE 0 END) AS occupiedSlots,
+	 SUM(CASE WHEN status = 'Reserved' THEN 1 ELSE 0 END) AS reservedSlots
+FROM parking_slot
+GROUP BY type;
